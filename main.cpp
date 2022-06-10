e09eb450458585efdd2ce361b3a655c392282630
hadar <hadarpinto@gmail.com>
2022-06-10 12:21:56 +0300

    neat and tidy

--- main.cpp
+++ main.cpp
@@ -28,7 +28,6 @@ public:
     int size;
     sem_t semEmpty;
     sem_t semFull;
-    int doneFlag;
 
     pthread_mutex_t mutexBuffer;
 
@@ -36,18 +35,15 @@ public:
         pthread_mutex_init(&mutexBuffer, NULL);
         sem_init(&semEmpty, 0, capacity);
         sem_init(&semFull, 0, 0);
-        doneFlag = 0;
     }
 
     void insert(string s) {
         sem_wait(&semEmpty);
         pthread_mutex_lock(&mutexBuffer);
-        this_thread::sleep_for(std::chrono::milliseconds(100));
         push(s);
-        this_thread::sleep_for(std::chrono::milliseconds(100));
         pthread_mutex_unlock(&mutexBuffer);
         sem_post(&semFull);
-        this_thread::sleep_for(std::chrono::milliseconds(100));
+
     }
 
     string remove() {
@@ -55,7 +51,6 @@ public:
         pthread_mutex_lock(&mutexBuffer);
         string x = front();
         pop();
-        this_thread::sleep_for(std::chrono::milliseconds(100));
         pthread_mutex_unlock(&mutexBuffer);
         sem_post(&semEmpty);
         return x;
@@ -71,16 +66,12 @@ public:
 
 class UnBoundedQ : public queue<string> {
 public:
-    int size;
     sem_t semFull;
-    int doneFlag;
-
     pthread_mutex_t mutexBuffer;
 
     explicit UnBoundedQ() {
         pthread_mutex_init(&mutexBuffer, NULL);
         sem_init(&semFull, 0, 0);
-        doneFlag = 0;
     }
 
     void insert(string s) {
@@ -125,12 +116,13 @@ string produce(int newNumber, int index) {
         return concatenate;
     }
 }
-
+/**
+ * Global vars
+ */
 
 vector<BoundedQ *> vecQs;
 vector<UnBoundedQ *> coQs;
 BoundedQ *boundedQ;
-atomic<int> done_count;
 
 void setToRightQ(string s) {
     //return -1 if not found
@@ -164,20 +156,14 @@ void *producer(void *args) {
         // Produce
         string s = produce(i, producerIndex);
         // Add to the buffer
-
         vecQs[producerIndex - 1]->insert(s);
         //count++;
         i++;
 
     }
-
     vecQs[producerIndex - 1]->insert("DONE");
-
 }
 
-// initialize a set of all indexes of producers. send from producer done. if we recieve done, we take out the
-// index from our set. when set size is equal to 0, we send done to our co -editors.
-// in function setrightq we send done to manger screen and it print done.
 void *dispatcher(void *args) {
     int sizeOfvector = *(int *) args;
     int done = sizeOfvector;
@@ -186,53 +172,43 @@ void *dispatcher(void *args) {
             if (vecQs[i] == nullptr) {
                 continue;
             }
-            string y;
-            // Remove from the buffer
-            y = vecQs[i]->remove();
-            // Consume
-            if (y == "DONE") {
+            string s;
+            s = vecQs[i]->remove();
+            if (s == "DONE") {
                 vecQs[i]->destroy();
                 vecQs[i] = nullptr;
                 done--;
                 continue;
             }
-            setToRightQ(y);
+            setToRightQ(s);
         }
     }
-
     setToRightQ("DONE");
-
-
 }
 
 void *coEditor(void *args) {
     int coEditorQueue = *(int *) args;
     cout << coEditorQueue << endl;
-    //int coEditorQueue = 0;
     while (1) {
         string s = coQs[coEditorQueue]->remove();
         if (s == "DONE") {
             boundedQ->insert(s);
             break;
         }
-
         boundedQ->insert(s);
     }
 }
 
 void *screenManger(void *args) {
     int done = 0;
-    std::mutex screen;
 
     while (1) {
-//        screen.lock();
         string s = boundedQ->remove();
         if (s != "DONE") {
             cout << s << endl;
         } else {
             done++;
         }
-//        screen.unlock();
         if (done == 3) {
             break;
         }
@@ -255,7 +231,6 @@ int main(int argv, char **argc) {
         }
         conf.close();
     }
-    done_count = producersNum;
     producersNum = producersNum / 4;
     // reading from conf file
     vector<struct confData> dataVector;
@@ -289,40 +264,10 @@ int main(int argv, char **argc) {
     }
     boundedQ = new BoundedQ(coEditorSize);
 
-    srand(time(NULL));
-    int totalThreads = producersNum + DISPATCHER + CO_EDITORS + SCREEN_MANAGER;
-    pthread_t th[totalThreads];
     pthread_t producers[producersNum];
-    pthread_t dispatc;
+    pthread_t dispatch;
     pthread_t coEditors[CO_EDITORS];
     pthread_t screen;
-//    int i;
-//    for (i = 0; i < totalThreads ; i++) {
-//        if (i == 0){
-//            if (pthread_create(&th[i], NULL, &dispatcher, &producersNum) != 0) {
-//                perror("Failed to create thread");
-//            }
-//        }
-//        else if (i > 0 && i<=producersNum) {
-//
-//            struct confData* cd = &dataVector[i-1];
-//            if (pthread_create(&th[i], NULL, &producer, cd) != 0) {
-//                perror("Failed to create thread");
-//            }
-//        } else if (i > producersNum && i<=producersNum+CO_EDITORS) {
-//
-//            int queueIndex = i - producersNum - 1;
-//            if (pthread_create(&th[i], NULL, &coEditor, &queueIndex) != 0) {
-//                perror("Failed to create thread");
-//            }
-//        }
-//        else {
-//
-//            if (pthread_create(&th[i], NULL, &screenManger, nullptr) != 0) {
-//                perror("Failed to create thread");
-//            }
-//        }
-//    }
 
     for (int i = 0; i < producersNum; i++) {
         sleep(0.11);
@@ -332,7 +277,7 @@ int main(int argv, char **argc) {
         }
     }
 
-    if (pthread_create(&dispatc, NULL, &dispatcher, &producersNum) != 0) {
+    if (pthread_create(&dispatch, NULL, &dispatcher, &producersNum) != 0) {
         perror("Failed to create thread");
     }
     sleep(1);
@@ -353,13 +298,6 @@ int main(int argv, char **argc) {
     if (pthread_create(&screen, NULL, &screenManger, nullptr) != 0) {
         perror("Failed to create thread");
     }
-//    sleep(1);
-
-//    for (i = 0; i < totalThreads; i++) {
-//        if (pthread_join(th[i], NULL) != 0) {
-//            perror("Failed to join thread");
-//        }
-//    }
 
     for (int i = 0; i < producersNum; i++) {
         if (pthread_join(producers[i], NULL) != 0) {
@@ -367,7 +305,7 @@ int main(int argv, char **argc) {
         }
     }
 
-    if (pthread_join(dispatc, NULL) != 0) {
+    if (pthread_join(dispatch, NULL) != 0) {
         perror("Failed to create thread");
     }
 
@@ -382,7 +320,5 @@ int main(int argv, char **argc) {
     }
 
     cout << "DONE" << endl;
-
     return 0;
-
 }
